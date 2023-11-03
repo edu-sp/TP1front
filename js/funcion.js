4adbdf0bcc68e94ed8733fadbc767806e54e597f
edu-sp <barsac14@gmail.com>
2023-11-03 10:33:14 -0300

    funcionalidad boton resumen

---
+++ js/funcion.js
@@ -0,0 +1,39 @@
+let valorTicket = 200;
+let option;
+let total;
+
+const categoria = document.querySelector("#categoria");
+const cantidad = document.getElementById("cantidad");
+const btnresumen = document.getElementById("btnresumen");
+const totaltext = document.getElementById("total");
+// evento change para cuando se elija una opcion de las categorias
+categoria.addEventListener("change", () => {
+  //  asignamos el nombre de la categoria elegida a la variable option
+  option = categoria.options[categoria.selectedIndex].text;
+  console.log(option);
+});
+
+
+btnresumen .addEventListener("click", () => {
+  switch (option) {
+    case "Estudiante":
+      total = 0;
+      total = cantidad.value * valorTicket * 0.2;
+      break;
+    case "Trainee":
+      total = 0;
+      total = cantidad.value * valorTicket * 0.5;
+      break;
+    case "Junior":
+      total = 0;
+      total = cantidad.value * valorTicket * 0.85;
+      break;
+    default:
+      total = cantidad.value * valorTicket;
+  }
+  totaltext.innerHTML = total;
+});
+
+function redirect() {
+  location.href = "tickets.html";
+}
